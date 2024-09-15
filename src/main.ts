c8734182e106a8368a9ecb7925ca7d8ce5e82182
Aymsep <71574687+Aymsep@users.noreply.github.com>
2024-09-15 22:56:04 +0000

    update env file

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import helmet from 'helmet';
 import { Logger } from '@nestjs/common';
 import { CustomLoggerService } from './logger/logger.service';
 import { HttpLoggingInterceptor } from './interceptors/http-logging.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -19,6 +20,19 @@ async function bootstrap() {
   app.useGlobalInterceptors(new HttpLoggingInterceptor(logger));
   app.use(compression());
   app.use(helmet());
+
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Collaborative Note-Taking API')
+      .setDescription('API documentation for the collaborative note-taking application')
+      .setVersion('1.0')
+      .addBearerAuth()  // Enable Bearer token authentication in Swagger
+      .build();
+    
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/v1/docs', app, document);
+    Logger.log('Swagger docs available at /api/v1/docs');
+  }
 
 
   await app.listen(3000, async() => {
